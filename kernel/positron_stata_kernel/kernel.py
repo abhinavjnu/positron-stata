5e3191cc53aafb069c6140c4f59afef218185530
Abhinav Kumar <abhinav@example.com>
2026-09-25 01:03:29 +0530

    release: v0.2.2 with simplified README and docs

--- kernel/positron_stata_kernel/kernel.py
+++ kernel/positron_stata_kernel/kernel.py
@@ -29,7 +29,7 @@ from .help_handler import StataHelpHandler
 
 class PositronStataKernel(Kernel):
     implementation = "positron_stata"
-    implementation_version = "0.2.1"
+    implementation_version = "0.2.2"
     language = "stata"
     language_version = os.environ.get("STATA_VERSION", "19")
     language_info = {
